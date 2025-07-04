2c9e561d62e9488d1be999c89a36a8fc734b6473
Haneul <sansadeer@naver.com>
2025-07-04 19:26:54 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="w-screen h-screen bg-gradient-to-br from-pink-400 to-purple-600 text-white p-10 text-center flex items-center justify-center flex-col">
+      <h1 className="text-3xl font-bold">Hello, World! ✨</h1>
+      <p className="mt-2">Tailwind! 🎉</p>
+    </div>
+  );
+}
+export default App;
+
